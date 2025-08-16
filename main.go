a823d20b5c6ce9db809af2f8133be4e0cb974bc4
Sergii Iefremov <se@gitpatch.com>
2025-08-16 02:25:11 -0400

    Send main ref

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/gliderlabs/ssh"
 )
 
+func pkt(s string) string {
+	return fmt.Sprintf("%04x%s\n", len(s)+5, s)
+}
+
 func main() {
 	ssh.Handle(func(s ssh.Session) {
-		fmt.Printf("Running: %s\n", s.RawCommand())
-		s.Write([]byte("ok"))
+		ref, err := os.ReadFile(".git/refs/heads/main")
+		if err != nil {
+			s.Exit(1)
+			return
+		}
+		ref = bytes.TrimRight(ref, "\n")
+
+		io.WriteString(s, pkt(fmt.Sprintf("%s HEAD\x00object-format=sha1 symref=HEAD:refs/heads/main", ref)))
+		io.WriteString(s, pkt(fmt.Sprintf("%s refs/heads/main", ref)))
+		io.WriteString(s, "0000")
 	})
 
 	log.Fatal(ssh.ListenAndServe(":2222", nil,
